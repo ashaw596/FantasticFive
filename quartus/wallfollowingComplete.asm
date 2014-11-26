d03f261fbb6f15050013a4b15e89bf97ae856996
Tyler Allen <interfior99@gmail.com>
2014-11-25 23:26:03 -0500

    Really, bad non working terrible wallfollowing code. Will get something that works better for tomorrow. I might just reccomend dead reckoning. I will get a more traditonal path planner. It should be able to be tested without a robot.

--- quartus/wallfollowingComplete.asm
+++ quartus/wallfollowingComplete.asm
@@ -43,7 +43,12 @@ MAIN:
 	JUMP DIE
 
 DIE:
-	
+	LOADI 	0
+	OUT 	LVELCMD
+	OUT 	RVELCMD
+	loadi	&hDEAD
+	out		lcd
+
 	JUMP DIE
 	
 ;USER COMMANDS
@@ -52,58 +57,130 @@ DIE:
 ;---------------------------------------------------------------------------------------------
 ;ASM CODE TO PERFORM WALL FOLLOWING ON THE COURSE
 ;WRITTEN BY TYLER ALLEN
-WALL_SONAR_MASK: 			DW 	&B00101000	;THE CORRECT BINARY MASK???
+WALL_SONAR_MASK: 			DW 	&B00101101	;THE CORRECT BINARY MASK???
 WALL_DISTANCE_SIDE: 		DW 	&Hd5		;THE DISTANCE THE ROBOT WILL FOLLOW FROM THE WALL
 WALL_DISTANCE_FORWARD: 		DW  &Hd5		;DISTANCE THE ROBOT IS INFRONT OF THE WALL AHEAD
 WALL_SONAR_MIN_VALUE:		DW 	&H88		;MIN VALUE THAT THE SONAR CAN READ
-WALL_SONAR_MAX_VALUE:		DW 	&HC00		;MAX VALUE THAT THE SONAR CAN READ
-WALL_BASE_SPEED:			DW 	300			;BASE SPEED THE ROBOT IS TRAVELING WITHOUT ERROR
+WALL_SONAR_MAX_VALUE:		DW 	&HC20		;MAX VALUE THAT THE SONAR CAN READ
+WALL_BASE_SPEED:			DW 	0			;BASE SPEED THE ROBOT IS TRAVELING WITHOUT ERROR
 WALL_MOTOR_MIN:				DW	-500		;MINIMUM SPEED OF MOTOR
 WALL_MOTOR_MAX:				DW	500			;MAX SPEED OF MOTOR
-WALL_TEMP:					DW  0
+WALL_FORWARD_ERROR:			DW	000			;STORE THE FORWARD ERROR FOR LATER
+WALL_END_BOUND:				dw	&H04F		;BOUND FOR SONAR ERROR MEASUREMENT TO END
+WALL_START_SQUARE:			DW	000			;START SQUARE FOR THE WALLFOLLOW
+WALL_SQUARE_DIST:			DW	&H22F		;SQUARE DIST TO MEASURE THE SQUARES INFRONT
+WALL_CURRENT_SQUARE:		DW	000			;CURRENT SQUARE
+WALL_BAD_SONAR_VALUE:		DW	&h7FFF		;BAD SONAR VALUE
+WALL_BAD_VALUE:				DW	0000		;SIGNALS IF A BAD VALUE HAS OCCURED
+WALL_IGNORE_BAD_VALUE:		DW	0000		;SIGNALS IF A BAD VALUE SHOULD BE CHECKED
+WALL_FOLLOW_DO_FAR:			DW	0000		;SIGNALS IF THE WALL FOLLOWING SHOULD FOLLOW FAR, IE IT SHOULD FOLLOW FROM ONE SQUARE AWAY. A SOLUTION TO THE SINGLE EDGE
+WAYPOINT1:					DW 	0000
+WAYPOINT2:					DW 	0000
+WAYPOINT3:					DW	0000
 WALLFOLLOW: 
-	LOAD 	WALL_SONAR_MASK
+	LOAD 	WALL_SONAR_MASK 
 	OUT  	SONAREN
-WALL_LOOP:
-	LOAD WALL_TEMP
-	OUT LCD
-	ADDI 1
-	STORE WALL_TEMP
+WALL_LOOP: 
 	;READ IN SONAR AND BOUND THE READINGS
+	LOAD 	WALL_FOLLOW_DO_FAR
+	JPOS	WALL_FOLLOW_FAR
+	;If wall follow is close to the wall ie. the robot is on a tile near the outside wall.
+	;Use the sonar sensor on the right of the robot
+WALL_FOLLOW_NEAR:
 	INA 	1, DIST5
+	JUMP 	WALL_CALC
+	;If the the robot is far from the wall use the sonar on the left 
+WALL_FOLLOW_FAR:
+	INA 	1, DIST0
+	;Perform wall following calculations 
+WALL_CALC:
+	;Bound sonar reading, if WALL_IGNORE_BAD_VALUE is true then ignore the bad value in the bounds 
+	LOADI	1
+	STORE 	WALL_IGNORE_BAD_VALUE
 	CALL 	WALL_BOUND_SOANR_READING
+	;Read the two front sonars and select the min on them
 	INA 	2, DIST3
-	SWITCH  1,2
+	INA 	3, DIST2
+	CALL WALL_MIN
+	;When min is found bound it to the correct value, always want to take into account bad values here
+	SWITCH  1,2 ;Switch the registers since WALL_BOUND_SOANR_READING expects the sonar reading to be in R1
+	LOADI	0
+	STORE 	WALL_IGNORE_BAD_VALUE
 	CALL 	WALL_BOUND_SOANR_READING
 
 	;CALCULATE ERROR VALUES 
 	;SIDE=R2,FORWARD=R1
+	CALL 		MeasureDIST0;Calculate squares infront of the robot
+	COPY		7,0
+	LOAD		WALL_START_POS
+	SUBA		0,7,0
+	;On clearer thinking this does not make sense on how to calculate the position
+	ADDA		0,7,0
+	OUT			LCD
+	STORE		WALL_CURRENT_SQUARE;Add to start square to find the current position
+	
+	;If bad value is detected don't do the error calculations, just move at a constant speed straight 
+	LOAD		WALL_BAD_VALUE
+	JPOS		WALL_SKIP_ERROR
+	;Calculate Error values 
 	LOAD 		WALL_DISTANCE_SIDE
-	SUBA		2,0,2
+	SUBA		2,2,0
 	
 	LOAD		WALL_DISTANCE_FORWARD
-	SUBA		1,0,1
+	SUBA		1,1,0
+	COPY		0,1
+	STORE		WALL_FORWARD_ERROR ;Store this error for later to use in the end check
+	OUTA		1, SSEG1
+
 	;CALCULATE MOTOR VALUES
 	;SIDE ERROR=R2, FORWARD ERROR=R1
+	;Calculate error values 
 	CALL	WALL_CALCULATE_MOTOR_VALUES
 	;R3=LEFT,R2=RIGHT
 	;APPLY MOTOR VALUES
-
-	OUTA 	3,LVELCMD
-	OUTA 	2,RVELCMD
+	;If the we reverse the direction of wall following the motor values have to be reversed 
+	LOAD 	WALL_FOLLOW_DO_FAR
+	JPOS	WALL_CALC_FAR
+WALL_CALC_NEAR:
+	OUTA 	2,LVELCMD
+	OUTA 	3,RVELCMD
+	JUMP  	WALL_LOOP_CHECK
+WALL_CALC_FAR:
+	OUTA	3,LVELCMD
+	OUTA	2,RVELCMD
+	;Check end conditions simply check if the sonar value is within WALL_END_BOUND 
+WALL_LOOP_CHECK:
 	CALL 	WALL_CHECK_END_COND
-	JPOS 	WALL_LOOP
+	JPOS	WALL_LOOP
 	JZERO	WALL_LOOP
-
+	;Return from function call 
+WALL_END_CALL:
 	RETURN
+	;Move at a set speed if a bad value on the sonar is detected to move past the error state
+WALL_SKIP_ERROR:
+	LOADI	&h1234
+	
+	LOADI	200
+	OUT		LVELCMD
+	OUT		RVELCMD
+	JUMP 	WALL_LOOP
 ;BOUNDS THE SONAR VALUES TO BE BETWEEN THE MIN<R1<MAX
 WALL_BOUND_SOANR_READING:
 	LOAD 	WALL_SONAR_MIN_VALUE
 	SUBA	0,1,0	;SUBTRACT THE MIN VALUE FROM SONAR
 	JNEG	WALL_BOUND_SMALL
+	LOAD 	WALL_IGNORE_BAD_VALUE
+	;Check for bad sonar values 
+	JPOS	WALL_CHECK_BOUND_LARGE
+	LOAD	WALL_BAD_SONAR_VALUE
+	SUBA	0,1,0
+	JPOS	WALL_BOUND_BAD
+WALL_CHECK_BOUND_LARGE:
 	LOAD 	WALL_SONAR_MAX_VALUE
 	SUBA	0,1,0 	;SUBTRACT THE MAX VALUE FROM SONAR 
 	JPOS	WALL_BOUND_LARGE
+	LOADI 	0
+	STORE 	WALL_BAD_VALUE
 	RETURN
 WALL_BOUND_SMALL:
 	LOAD	WALL_SONAR_MIN_VALUE
@@ -113,29 +190,35 @@ WALL_BOUND_LARGE:
 	LOAD	WALL_SONAR_MAX_VALUE
 	COPY 1,0
 	RETURN
+WALL_BOUND_BAD:
+	LOADI 1
+	STORE WALL_BAD_VALUE
+	RETURN
 ;CALCULATE MOTOR VALUES BASED ON SONAR ERROR VALUES 
 ;R2=SIDE ERROR,R1=FORWARD ERROR
+;R6,R7=Free, returns on ->R2,R3
 WALL_CALCULATE_MOTOR_VALUES:
 	LOAD WALL_BASE_SPEED
 	COPY 5,0
-	COPY 0,1
-	ADDA 6,5,0
+	COPY 0,1 ;load error into a(0) to apply gains
+	SHIFT -1 ;adjust gains 
+	ADDA 6,5,0 ;Add to motor value registers
 	ADDA 7,5,0
-
-	COPY 	1,6
-	CALL 	WALL_BOUND_MOTOR_VALUES 
-	COPY 	6,1
-	COPY 	1,7
+	;Bound the forward motion
+	COPY 	1,6 					;Copy the motor value into R1 to call WALL_BOUND_MOTOR_VALUES
+	CALL 	WALL_BOUND_MOTOR_VALUES ;BOund motor values on to ensure that the motor values are good
+	COPY 	6,1 					;Copy the motor value back int R6 for temp storage
+	COPY 	1,7 
 	CALL 	WALL_BOUND_MOTOR_VALUES 
 	COPY 	7,1
-	
+	;Begin calculating the motor the turns based on distance from side wall
 	COPY 0,2
-	ADDA 6,6,0
-	SUBA 7,7,0
-	COPY 2,6
+	SHIFT -1	;Apply gains 
+	ADDA 6,6,0	;Add to motor values on the side facing  the wall
+	SUBA 7,7,0	;Sub to motor values on the side away from the wall
+	COPY 2,6	;Put into r2,r3 to output
 	COPY 3,7
-	
-
+	;Bound motor values to ensure they are legal motor values
 	COPY 	1,2
 	CALL 	WALL_BOUND_MOTOR_VALUES 
 	COPY 	2,1
@@ -143,8 +226,6 @@ WALL_CALCULATE_MOTOR_VALUES:
 	CALL 	WALL_BOUND_MOTOR_VALUES 
 	COPY 	3,1
 	
-	OUTA	2, SSEG1
-	OUTA	3, SSEG2
 
 	RETURN 
 ;BOUND THE MOTOR VALUES TO BE BETWEEN MIN<MOTOR<MAX
@@ -167,9 +248,66 @@ WALL_MOTOR_LARGE:
 	RETURN
 ;WALL CHECK IF END CONDITION IS MET 
 WALL_CHECK_END_COND:
-	LOADI 1
+	LOAD	WALL_FORWARD_ERROR ;Load error 
+	JNEG	WALL_END_NEG		;Check if error is negative
+	COPY 1,0	
+	;If not negative subtract WALL_END_BOUND from the error to see if the error is reasonably close to zero 
+WALL_END_CHECK:
+	LOAD 	WALL_END_BOUND
+	SUBA	0,1,0
+	;OUT 	LCD
 	RETURN	
+	;If negative invert the error reading to make the calculation work
+WALL_END_NEG:
+	COPY	1,0
+	LOADI 	0
+	SUBA		1,0,1
+	JUMP WALL_END_CHECK
+;Find min of R2 and R3 and returns on R2
+;R2,R3->R2
+WALL_MIN:
+	SUBA 0,2,3
+	JPOS WALL_MIN_3
+	return
+WALL_MIN_3:
+	COPY 2,3
+	return
 ;--------------------------------------------------------------------------------------	
+; Motion thresholds
+diff0:    DW &H22F
+diff1:    DW &H482
+diff2:    DW &H6ED
+diff3:    DW &H929
+diff4:    DW &HB94
+MeasureDIST0:
+tiles00:	IN 		DIST0
+		SUB		diff0
+		JPOS	tiles01
+		LOADI   0
+		RETURN
+tiles01:	IN		DIST0
+		SUB		diff1
+		JPOS	tiles02
+		LOADI	1
+		RETURN
+tiles02:	IN		DIST0
+		SUB		diff2
+		JPOS	tiles03
+		LOADI	2
+		RETURN
+tiles03:	IN		DIST0
+		SUB		diff3
+		JPOS	tiles04
+		LOADI	3
+		RETURN
+tiles04:	IN		DIST0
+		SUB		diff4
+		JPOS	tiles05
+		LOADI	4
+		RETURN
+tiles05: LOADI   5
+		RETURN
+
 Wait1:
 	OUT    TIMER	
 BattCheck:
